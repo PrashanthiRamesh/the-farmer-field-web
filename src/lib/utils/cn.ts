5c8098e2795d8f7237e8ca511b7e9262346bcf5a
Prashanthi Ramesh <rvprashanthi@gmail.com>
2024-12-29 19:21:06 -0500

    Import harmony color palette

--- src/lib/utils/cn.ts
+++ src/lib/utils/cn.ts
@@ -1,6 +1,23 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from "../../../tailwind.config";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const fullConfig = resolveConfig(tailwindConfig);
+
+export const getTailwindColor = (colorName: string): string | undefined => {
+  const [prefix, colorKey] = colorName.split(':');
+  const colors = fullConfig.theme?.colors as Record<string, any>;
+  if (colors && prefix && colorKey) {
+      const colorGroup = colors[prefix];
+      if (colorGroup && typeof colorGroup === 'object') {
+          return colorGroup[colorKey];
+      }
+  }
+
+  return undefined;
+};
