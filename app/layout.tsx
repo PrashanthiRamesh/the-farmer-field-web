30088e6e0322b1766b3b5ded3feca8f89eb6515c
Prashanthi Ramesh <rvprashanthi@gmail.com>
2025-02-22 23:57:45 +0530

    Setup
    https://www.youtube.com/watch?v=FTH6Dn3AyIQ

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Playfair_Display } from "next/font/google";
+import "./globals.css";
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "The Farmer Field",
+  description: "Our farms to your home",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${playfairDisplay.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
