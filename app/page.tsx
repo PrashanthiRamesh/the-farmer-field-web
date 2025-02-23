ffe047d3602a84a4f50ab5ba2cbab078eafe5c36
Prashanthi Ramesh <rvprashanthi@gmail.com>
2025-02-23 14:47:58 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Intro from "@/components/Intro";
 
 export default function Home() {
   return (
-    <main className="relative bg-seasalt flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative bg-seasalt flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <div className="w-full">
         <Intro />
       </div>
     </main>
