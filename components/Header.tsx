583dcf4b6077b6549ae510a9fb8608876ae72c7b
Prashanthi Ramesh <rvprashanthi@gmail.com>
2025-04-19 01:53:22 +0530

    Update navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,31 @@
 import Link from 'next/link'
 import { Button } from './ui/button'
 import React from 'react'
+import Nav from './Nav'
 
 const Header = () => {
     return (
         <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto">
+            <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
                     <h1>
                         <p className='text-4xl font-semibold'>The Farmer Field</p>
                         <p className={`text-2xl text-accent font-secondary`}>Our farms to your home</p>
                     </h1>
                 </Link>
+
+                {/* Desktop Navigation */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/subscribe">
+                        <Button>Subscribe</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile Navigation */}
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
             </div>
         </header>
     )
